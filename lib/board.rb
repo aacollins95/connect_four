8715e8e00d1239feafea2ff2b6794719eb6a8097
Aaron Collins <aa.collins.95@gmail.com>
2019-04-11 21:53:37 -0400

    refactored check_dirs

--- lib/board.rb
+++ lib/board.rb
@@ -10,14 +10,13 @@ class Board
   def check_win(col)
     root = last_placed(col)
     pointers = root.match_dirs
-    puts pointers
-    # if pointers.length > 0
-    #   connect_4s = 0
-    #   pointers.each { |pos,dir| connect_4s += 1 if root.check_line(dir) }
-    #   return connect_fours > 0
-    # else
-    #   return false
-    # end
+    if pointers.length > 0
+      connect_4s = 0
+      pointers.each { |pos,dir| connect_4s += 1 if root.check_line(dir) }
+      return connect_4s > 0
+    else
+      return false
+    end
   end
 
 
@@ -89,37 +88,33 @@ class Slot
   end
 
   def check_dir(dir)
-    #gets directions where there's a match adjacent to root
-    #checks a match for a connect four in a direction
+    #gets the number of matches in a given direction
     matches = 0
     player = @status
-    node = self
-    runs = 0
-    until node.status != player || runs > 2
-      runs += 1
-      next_pos = node.advance(dir)
-      @@slots.each do |slot|
-        if next_pos == slot.pos
-          matches += 1
-          node = slot
-        end
-      end
-      puts node.status != player
+    next_pos = advance(dir)
+    next_slot = @@slots[next_pos]
+    while on_board(next_pos) && @@slots[next_pos].status == player
+      matches += 1
+      next_pos = next_slot.advance(dir)
+      next_slot = @@slots[next_pos] if on_board(next_pos)
     end
     return matches
   end
 
   def check_line(dir)
+    #checks matches in the dir and reverse dir
     puts check_dir(dir)
     return false
   end
 
   def advance(dir)
+    #naive advance in a given direction
     next_pos = [@pos[0]+dir[0],@pos[1]+dir[1]]
     return next_pos
   end
 
   def on_board(pos)
+    #checks if a position is on the board
     height, width = 6,7
     return (0 <= pos[0] && pos[0] < width && 0 <= pos[1] && pos[1] < height)
   end
